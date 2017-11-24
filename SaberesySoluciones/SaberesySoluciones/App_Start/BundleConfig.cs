3c4fce9ddbcc0358a783cf07ae1382afc2cc44a2
Daniel Pavez <kirito.furukawa@gmail.com>
2017-11-24 08:48:20 -0300

    Agregue la vista aprendizaje en el proyecto

--- SaberesySoluciones/SaberesySoluciones/App_Start/BundleConfig.cs
+++ SaberesySoluciones/SaberesySoluciones/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace SaberesySoluciones
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/material.css",
+                      "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
     }
