62c63e0f8429f53f04bd94d051976771a44ec866
Daniela Paredes <elynawatcher@gmail.com>
2017-11-28 19:40:48 -0300

    Revert "Merge from Kazuto"
    This reverts commit dd78045ca13d3e8d08372188adcac36b00f14d60.

--- SaberesySoluciones/SaberesySoluciones/Repositories/Aprendizaje.cs
+++ SaberesySoluciones/SaberesySoluciones/Repositories/Aprendizaje.cs
@@ -7,34 +7,5 @@ namespace SaberesSyllabus.Repositories
 {
     public class Aprendizaje
     {
-        public static Aprendizaje Crear(Aprendizaje aprendizaje)
-        {
-            return null;
-        }
-
-        public static bool Editar(Aprendizaje aprendizaje)
-        {
-            return false;
-        }
-
-        public static bool Habilitar(Aprendizaje aprendizaje)
-        {
-            return false;
-        }
-
-        public static bool Deshabilitar(Aprendizaje aprendizaje)
-        {
-            return false;
-        }
-
-        public static Aprendizaje Leer(string codigo)
-        {
-            return null;
-        }
-
-        public static List<Aprendizaje> LeerTodo()
-        {
-            return null;
-        }
     }
 }
