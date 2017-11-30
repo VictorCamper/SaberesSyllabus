ed6e3d26b9a6093aead96ca38862603ec1cae269
Daniel Pavez <kirito.furukawa@gmail.com>
2017-11-29 22:54:02 -0300

    Modals de ejemplo en vista Aprendizajes

--- SaberesySoluciones/SaberesySoluciones/App_Start/BundleConfig.cs
+++ SaberesySoluciones/SaberesySoluciones/App_Start/BundleConfig.cs
@@ -20,12 +20,11 @@ namespace SaberesySoluciones
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootstrap.bundle.min.js",
                       "~/Scripts/fontawesome5.js",
                       "~/Scripts/respond.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+            bundles.Add(new StyleBundle("~/Content/css").Include(                      
                       "~/Content/materia.css",
                       "~/Content/fontawesome-pro-core.css",
                       "~/Content/site.css"));
