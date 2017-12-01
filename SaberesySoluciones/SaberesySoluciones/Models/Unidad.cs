7b3a89b8679f49aa7fc917e88d329e7f0414fd6f
naxo <icorrea12@alumnos.utalca.cl>
2017-11-30 23:06:52 -0300

    Vistas del crud de Unidad
    Modals de listar clases y saberes
    se creo la conexión con la base de datos de forma local
    se intento realizar el listar de los datos desde la base de datos hacia la interfas pero no funciono :(

--- SaberesySoluciones/SaberesySoluciones/Models/Unidad.cs
+++ SaberesySoluciones/SaberesySoluciones/Models/Unidad.cs
@@ -1,6 +1,9 @@
-﻿using SaberesySoluciones.Models;
+﻿using MySql.Data.MySqlClient;
+using SaberesySoluciones.Models;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
 using System.Linq;
 using System.Web;
 
@@ -17,8 +20,104 @@ namespace SaberesSyllabus.Models
         {
             this.clases = new List<Clase>();
             this.saberes = new List<Saber>();
-            this.evaluaciones = new List<Evaluacion>();            
+            this.evaluaciones = new List<Evaluacion>();
         }
+
+        public bool Crear()
+        {
+            using (var conn = new MySqlConnection(ConfigurationManager.ConnectionStrings["Syllabus"].ConnectionString))
+            {
+                conn.Open();
+                var sqlTran = conn.BeginTransaction();
+                try
+                {
+                    var command = new MySqlCommand() { CommandText = "crearUnidad", CommandType = CommandType.StoredProcedure };
+                    //Setea el valor de los atributos del SP (procedimiento almacenado)
+                    command.Parameters.AddWithValue("titulo", this.titulo);
+
+                    command.Connection = conn;
+                    command.Transaction = sqlTran;
+                    command.ExecuteNonQuery();
+                    sqlTran.Commit();
+                    conn.Close();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                    sqlTran.Rollback();
+                }
+            }
+            return true;
+        }
+
+        public List<Unidad> ListarTodos()
+        {
+            var sucursales = new List<Unidad>();
+            try
+            {
+                var ds = new System.Data.DataSet();
+                using (var conn = new MySqlConnection(ConfigurationManager.ConnectionStrings["Syllabus"].ConnectionString))
+                {
+                    var command = new MySqlCommand() { CommandText = "getTitulo", CommandType = CommandType.StoredProcedure };
+                    conn.Open();
+                    command.Connection = conn;
+                    var sqlda = new MySqlDataAdapter(command);
+                    sqlda.Fill(ds);
+                    conn.Close();
+                }
+                foreach (DataRow dr in ds.Tables[0].Rows)
+                {
+                    Unidad s = new Unidad();
+                    s.CargarDatos(dr);
+                    sucursales.Add(s);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return sucursales;
+        }
+
+        public void Seleccionar(String id)
+        {
+            try
+            {
+                var ds = new DataSet();
+                using (var conn = new MySqlConnection(ConfigurationManager.ConnectionStrings["Syllabus"].ConnectionString))
+                {
+                    var command = new MySqlCommand() { CommandText = "getTitulo", CommandType = CommandType.StoredProcedure };
+                    command.Parameters.AddWithValue("titulo", titulo);
+                    conn.Open();
+                    command.Connection = conn;
+                    var sqlda = new MySqlDataAdapter(command);
+                    sqlda.Fill(ds);
+                    conn.Close();
+                }
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+                    this.CargarDatos(ds.Tables[0].Rows[0]);
+                    return;
+                }
+                else
+                {
+                    this.titulo = "Not Found";
+
+                }
+            }
+            catch (Exception ex)
+            {
+                //notificar administrador
+                throw ex;
+            }
+        }
+
+        public void CargarDatos(DataRow dr)
+        {
+
+            this.titulo = dr["titulo"].ToString();
+
+        }
+
     }
 }
-
