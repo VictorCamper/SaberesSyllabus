75462f6f0b9bc8cb47f8d022714b3753e684b04e
Daniela Paredes <elynawatcher@gmail.com>
2017-12-01 02:23:22 -0300

    Add procedures

---
+++ DataBase/sp_competencias_crear.sql
@@ -0,0 +1,21 @@
+DELIMITER $$
+DROP PROCEDURE IF EXISTS sp_competencias_crear $$
+CREATE PROCEDURE sp_competencias_crear
+(
+	in_descripcion VARCHAR(256),
+  	in_nivel_dominio VARCHAR(256),
+  	in_basico TEXT,
+  	in_intermedio TEXT,
+  	in_avanzado TEXT,
+  	in_tiempoDesarrollo VARCHAR(256),
+  	in_estado VARCHAR(256)
+	OUT out_codigo bigint
+)
+BEGIN
+
+	INSERT INTO competencia(descripcion, nivel_dominio, basico, intermedio, avanzado, tiempoDesarollo, estado)
+	VALUES (in_descripcion, in_nivel_dominio, in_basico, in_intermedio, in_avanzado, in_tiempoDesarrollo, in_estado);
+
+	SET out_codigo = LAST_INSERT_ID();
+END
+$$
