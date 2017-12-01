1033637d460075e1c62b83af7d729adfc9080b72
Victor Reyes Medina <victor.reyes.medina@gmail.com>
2017-12-01 01:25:41 -0300

    Vistas evaluacion

--- SaberesySoluciones/SaberesySoluciones/Models/Evaluacion.cs
+++ SaberesySoluciones/SaberesySoluciones/Models/Evaluacion.cs
@@ -4,6 +4,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+//Base de datos
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.Configuration;
+
 namespace SaberesSyllabus.Models
 {
     public class Evaluacion
@@ -15,5 +20,36 @@ namespace SaberesSyllabus.Models
         {
             this.saberes = new List<Saber>();
         }
+
+        /*public bool Crear()
+        {
+            using (var conn = new MySqlConnection(ConfigurationManager.ConnectionStrings["algo va aqui"].ConnectionString))
+            {
+                conn.Open();
+                var sqlTran = conn.BeginTransaction();
+                try
+                {
+                    var command = new MySqlCommand()
+                    {
+                        CommandText = "crearSucursal",
+                        CommandType = CommandType.StoredProcedure
+                    };
+                    //Setea el valor de los atributos del SP (procedimiento almacenado)
+                    command.Parameters.AddWithValue("nombre", this.Nombre);
+                    command.Parameters.AddWithValue("direccion", this.Direccion);
+                    command.Parameters.AddWithValue("estado", this.Estado);
+                    command.Connection = conn;
+                    command.Transaction = sqlTran;
+                    command.ExecuteNonQuery();
+                    sqlTran.Commit();
+                    conn.Close();
+                }
+                catch (Exception ex)
+                {
+                    sqlTran.Rollback();
+                }
+            }
+            return true;
+        }*/
     }
 }
