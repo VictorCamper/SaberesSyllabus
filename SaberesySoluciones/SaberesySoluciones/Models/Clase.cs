f37eac025e22afc3e6ebb407a457cb2d36bc9c92
Gustafox <gurojas13@alumnos.utalca.cl>
2017-12-05 12:00:49 -0300

    Operaciones a medias
    crear y listar funcionando a medias

--- SaberesySoluciones/SaberesySoluciones/Models/Clase.cs
+++ SaberesySoluciones/SaberesySoluciones/Models/Clase.cs
@@ -2,6 +2,8 @@
 using SaberesSyllabus.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Configuration;
 using System.Data;
 using System.Linq;
@@ -11,17 +13,48 @@ namespace SaberesySoluciones.Models
 {
     public class Clase
     {
+        public long codigo { get; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime fecha { get; set; }
         public string horario { get; set; }
         public string tema { get; set; }
         public string descripcion { get; set; }
         public List<Saber> saberes { get; set; }
-        public EnumTipoClase tipo { get; set; }
+        public TipoClase tipo { get; set; }
 
         public Clase()
         {
             this.saberes = new List<Saber>();
         }
+
+        public bool Crear()
+        {
+            using (var conn = new MySqlConnection(ConfigurationManager.ConnectionStrings["Syllabus"].ConnectionString))
+            {
+                conn.Open();
+                var sqlTran = conn.BeginTransaction();
+                try
+                {
+                    var command = new MySqlCommand() { CommandText = "crearClase", CommandType = CommandType.StoredProcedure };
+                    //Setea el valor de los atributos del SP (procedimiento almacenado)
+                    command.Parameters.AddWithValue("fecha", this.fecha);
+                    command.Parameters.AddWithValue("tema", this.tema);
+                    command.Parameters.AddWithValue("descripcion", this.descripcion);
+                    command.Connection = conn;
+                    command.Transaction = sqlTran;
+                    command.ExecuteNonQuery();
+                    sqlTran.Commit();
+                    conn.Close();
+                }
+                catch (Exception ex)
+                {
+                    sqlTran.Rollback();
+                }
+            }
+            return true;
+        }
+
 
         public List<Clase> ListarClases()
         {
@@ -60,9 +93,94 @@ namespace SaberesySoluciones.Models
         {
             this.fecha = (DateTime)dr["fecha"];
             //this.horario = dr["nombre"].ToString();
-            this.horario = String.Concat(dr["horaInicio"].ToString(), " - ", dr["horaTermino"]);
+            //this.horario = String.Concat(dr["horaInicio"].ToString(), " - ", dr["horaTermino"]);
             this.tema = dr["tema"].ToString();
             this.descripcion = dr["descripcion"].ToString();
+            //this.tipo = (TipoClase)Convert.ToInt32(dr["tipoClase"].ToString());
+
         }
+
+        public void Seleccionar(long id)
+        {
+            try
+            {
+                var ds = new DataSet();
+                using (var conn = new
+                MySqlConnection(ConfigurationManager.ConnectionStrings["Syllabus"].ConnectionString))
+                {
+                    var command = new MySqlCommand()
+                    {
+                        CommandText = "getClaseID",
+                        CommandType =
+                    CommandType.StoredProcedure
+                    };
+                    command.Parameters.AddWithValue("id", this.codigo);
+                    conn.Open();
+                    command.Connection = conn;
+                    var sqlda = new MySqlDataAdapter(command);
+                    sqlda.Fill(ds);
+                    conn.Close();
+                }
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+                    this.CargarDatos(ds.Tables[0].Rows[0]);
+                }
+                else
+                {
+                    /*
+                    this.Nombre = "Not Found";
+                    this.Direccion = "Not Found";
+                    this.Estado = "Not Found";
+                    this.Id = -1;
+                    */
+                }
+            }
+            catch (Exception ex)
+            {
+                //notificar administrador
+                throw ex;
+            }
+        }
+    
+
+        public void EliminarSucursal(long codigo)
+        {
+            try
+            {
+                using (var conn = new
+                MySqlConnection(ConfigurationManager.ConnectionStrings["Syllabus"].ConnectionString))
+                {
+                    var command = new MySqlCommand()
+                    {
+                        CommandText = "eliminarClase",
+                        CommandType = CommandType.StoredProcedure
+                    };
+
+                    command.Parameters.AddWithValue("id", this.codigo);
+                    command.Connection = conn;
+                    conn.Open();
+                    command.ExecuteNonQuery();
+                    conn.Close();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                //notificar administrador
+                throw ex;
+            }
+        }
+
+
+    }
+
+    public enum TipoClase
+    {
+        [Description("Clase")]
+        Clase,
+        [Description("Laboratorio")]
+        Laboratorio,
+        [Description("Ayudantia")]
+        Ayudantia,
     }
 }
