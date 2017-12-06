4815bc3d8ac682dd1f6746cc74420c15799a1de2
Rodolfo <rhenzi13@alumnos.utalca.cl>
2017-12-06 19:52:51 -0300

    Programa masisis unido

---
+++ SaberesySoluciones/SaberesySoluciones/Controllers/PrincipalController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SaberesySoluciones.Models;
+
+namespace SaberesySoluciones.Controllers
+{
+    public class PrincipalController : Controller
+    {
+        // GET: Principal
+        public ActionResult Index()
+        {
+            ViewBag.title = "Principal";
+            var principal = new Principal();
+            return View(principal);
+        }
+
+        [HttpGet]
+        public ActionResult CrearPrincipal()
+        {
+            Principal principal = new Principal();
+            return View(principal);
+        }
+
+        [HttpPost]
+        public ActionResult CrearPrincipal(Principal nueva)
+        {
+            //nueva.Crear();
+            return RedirectToAction("Index");
+
+        }
+    }
+}
---
+++ SaberesySoluciones/SaberesySoluciones/Models/Principal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SaberesySoluciones.Models
+{
+    public class Principal
+    {
+
+        public Principal()
+        {
+
+        }
+
+        public void Crear()
+        {
+
+        }
+    }
+}
