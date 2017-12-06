9529caf878981c3c0cfbc6ab163bb6ec0cbb35f1
Daniela Paredes <elynawatcher@gmail.com>
2017-12-05 23:51:03 -0300

    Add AprendizajeEnSaber

---
+++ SaberesySoluciones/SaberesySoluciones/ViewModel/AprendizajeEnSaberesController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SaberesySoluciones.Models;
+
+namespace SaberesySoluciones.ViewModel
+{
+    public class AprendizajeEnSaber
+    {
+        public List<Aprendizaje> aprendizajes { get; set; }
+        public List<Saber> saberes { get; set; }
+    }
+}
