555606a04ab6d0bd19a201119b8f7f7d43141f1d
Daniela Paredes <elynawatcher@gmail.com>
2017-12-06 08:39:25 -0300

    Up LoQueSeMePaso

--- SaberesySoluciones/SaberesySoluciones/Models/Competencia.cs
+++ SaberesySoluciones/SaberesySoluciones/Models/Competencia.cs
@@ -9,7 +9,7 @@ namespace SaberesySoluciones.Models
     {
         public int Codigo { get; set; }
         public string Nombre { get; set;}
-        public string descripcion { get; set; }
+        public string Descripcion { get; set; }
         public EnumNivelDominio Dominio { get; set;}
         public string Basico { get; set; }
         public string Intermedio { get; set; }
