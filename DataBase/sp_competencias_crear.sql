609049ca4605f17026f891a3eb80ae1511f29bd5
Daniel Pavez <kirito.furukawa@gmail.com>
2017-12-06 14:58:50 -0300

    Fix Competencia

--- DataBase/sp_competencias_crear.sql
+++ DataBase/sp_competencias_crear.sql
@@ -10,12 +10,13 @@ CREATE PROCEDURE sp_competencias_crear
   	in_avanzado TEXT,
   	in_tiempoDesarrollo VARCHAR(256),
   	in_estado VARCHAR(256),
+    in_porcentajeLogro INTEGER,
 	OUT out_codigo INTEGER
 )
 BEGIN
 
-	INSERT INTO competencia(nombre, descripcion, dominio, basico, intermedio, avanzado, tiempoDesarrollo, estado)
-	VALUES (in_nombre ,in_descripcion, in_dominio, in_basico, in_intermedio, in_avanzado, in_tiempoDesarrollo, in_estado);
+	INSERT INTO competencia(nombre, descripcion, dominio, basico, intermedio, avanzado, tiempoDesarrollo, estado, porcentajeLogro)
+	VALUES (in_nombre ,in_descripcion, in_dominio, in_basico, in_intermedio, in_avanzado, in_tiempoDesarrollo, in_estado, in_porcentajeLogro);
 
 	SET out_codigo = LAST_INSERT_ID();
 END
