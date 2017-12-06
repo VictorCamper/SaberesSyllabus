2f95f0579cb25484ff06c5f068211190bc78405a
Daniela Paredes <elynawatcher@gmail.com>
2017-12-06 09:43:33 -0300

    Update procedure bd
    Arreglado almacenado hecho por el juan porque estaba desactualizado y malo.

--- DataBase/sp_competencias_crear.sql
+++ DataBase/sp_competencias_crear.sql
@@ -4,7 +4,7 @@ CREATE PROCEDURE sp_competencias_crear
 (
     in_nombre TEXT NOT NULL,
     in_descripcion VARCHAR(256),
-  	in_nivel_dominio VARCHAR(256),
+  	in_dominio VARCHAR(256),
   	in_basico TEXT,
   	in_intermedio TEXT,
   	in_avanzado TEXT,
@@ -14,8 +14,8 @@ CREATE PROCEDURE sp_competencias_crear
 )
 BEGIN
 
-	INSERT INTO competencia(nombre, descripcion, nivel_dominio, basico, intermedio, avanzado, tiempoDesarrollo, estado)
-	VALUES (in_nombre ,in_descripcion, in_nivel_dominio, in_basico, in_intermedio, in_avanzado, in_tiempoDesarrollo, in_estado);
+	INSERT INTO competencia(nombre, descripcion, dominio, basico, intermedio, avanzado, tiempoDesarrollo, estado)
+	VALUES (in_nombre ,in_descripcion, in_dominio, in_basico, in_intermedio, in_avanzado, in_tiempoDesarrollo, in_estado);
 
 	SET out_codigo = LAST_INSERT_ID();
 END
