c253051a90f19deeb5d27d237456bc82db0a1e93
Daniela Paredes <elynawatcher@gmail.com>
2017-12-15 00:39:55 -0300

    Fixed quien sabe que cosa

--- DataBase/insertUnidadEvaluacion.sql
+++ DataBase/insertUnidadEvaluacion.sql
@@ -1,5 +1,5 @@
-INSERT INTO Unidad(id, titulo) VALUES (1, 'Unidad 1');
-INSERT INTO Unidad(id, titulo) VALUES (2, 'Unidad 2');
-INSERT INTO Unidad(id, titulo) VALUES (3, 'Unidad 3');
+INSERT INTO Unidad(id, titulo, idCurso) VALUES ('1', 'Unidad 1', '1');
+INSERT INTO Unidad(id, titulo, idCurso) VALUES ('2', 'Unidad 2', '1');
+INSERT INTO Unidad(id, titulo, idCurso) VALUES ('3', 'Unidad 3', '1');
 INSERT INTO Evaluacion(tipoEvaluacion) VALUES ('Prueba');
-INSERT INTO Unidad_Evaluacion(idUnidad, idEvaluacion) VALUES (1,1);
+INSERT INTO Unidad_Evaluacion(idUnidad, idEvaluacion) VALUES ('1','1');
